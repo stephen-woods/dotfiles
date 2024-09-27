812b10019c8b177f78f7813676e0de1e031cf531
Stephen Woods <stephen_woods@cable.comcast.com>
2024-09-26 22:35:55 -0400

    neovim telescope buffers new options for MRU

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -92,6 +92,7 @@ return {
       require("telescope").load_extension("dap")
 
       local builtin = require('telescope.builtin')
+      local buffers_opts = { sort_mru = true, ignore_current_buffer = true, sort_lastused = true }
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
@@ -108,7 +109,7 @@ return {
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch existing [B]uffers' })
+      vim.keymap.set('n', '<leader>sb', function() builtin.buffers(buffers_opts) end, { desc = '[S]earch existing [B]uffers' })
       vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, { desc = '[S]earch LSP document [S]ymbols' })
       vim.keymap.set('n', '<leader>sS', builtin.lsp_dynamic_workspace_symbols, { desc = '[S]earch LSP workspace [S]ymbols' })
       vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzy find in current buffer' })
