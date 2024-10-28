f07e0780f436837891eb762eeebd8c810866d270
Stephen Woods <stephen_woods@cable.comcast.com>
2024-10-27 23:39:15 -0400

    Disable hints for gopls

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -19,12 +19,12 @@ return {
               },
               hints = {
                 assignVariableTypes = false,
-                compositeLiteralFields = true,
-                compositeLiteralTypes = true,
-                constantValues = true,
-                functionTypeParameters = true,
-                parameterNames = true,
-                rangeVariableTypes = true,
+                compositeLiteralFields = false,
+                compositeLiteralTypes = false,
+                constantValues = false,
+                functionTypeParameters = false,
+                parameterNames = false,
+                rangeVariableTypes = false,
               },
               analyses = {
                 fieldalignment = true,
