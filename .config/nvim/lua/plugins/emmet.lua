051d0aa08ecae080cdfedb3123c0314c646bfd7e
Stephen Woods <stephen_woods@cable.comcast.com>
2025-01-20 22:17:25 -0500

    Switched to emmet-language-server

--- .config/nvim/lua/plugins/emmet.lua
+++
@@ -1,8 +0,0 @@
--- ~/.config/nvim/lua/plugins/emmet.lua
-return {
-  "mattn/emmet-vim",
-  ft = { "html", "css", "javascriptreact", "typescriptreact", "vue", "svelte" },
-  -- config = function()
-  --   vim.g.user_emmet_leader_key = "<C-e>" -- Sets the Emmet trigger key to Ctrl+e
-  -- end,
-}
---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,24 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "angular-language-server",
+      "codelldb",
+      "emmet-language-server",
+      "eslint-lsp",
+      "gofumpt",
+      "goimports",
+      "gopls",
+      "helm-ls",
+      "json-lsp",
+      "lua-language-server",
+      "prettier",
+      "shfmt",
+      "sqlfluff",
+      "stylua",
+      "taplo",
+      "vtsls",
+      "yaml-language-server",
+    },
+  },
+}
